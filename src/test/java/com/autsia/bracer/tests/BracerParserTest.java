deaee72d9442fd01905d1a2ec9544936ef44c7b1
Dmytro Titov <dmytro.titov@gmail.com>
2014-01-11 19:31:50 +0200

    Minor refactoring.

--- src/test/java/com/autsia/bracer/tests/BracerParserTest.java
+++ src/test/java/com/autsia/bracer/tests/BracerParserTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Dmytro Titov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.autsia.bracer.tests;
 
 import java.util.Collection;
@@ -78,9 +94,9 @@ public class BracerParserTest {
         bracerParser.parse("( ( true and ( false or ( true and ( ( true ) or ( false ) ) ) ) ) and ( ( false ) ) )");
         Assert.assertEquals("0", bracerParser.evaluate());
 
-        Collection< String > stackRPN = bracerParser.getStackRPN();
+        Collection<String> stackRPN = bracerParser.getStackRPN();
         Assert.assertThat(stackRPN,
-            IsIterableContainingInOrder.contains(new String[] {"&", "0", "&", "|", "&", "|", "0", "1", "1", "0", "1"}));
+                IsIterableContainingInOrder.contains("&", "0", "&", "|", "&", "|", "0", "1", "1", "0", "1"));
     }
 
     @Test
@@ -94,8 +110,8 @@ public class BracerParserTest {
         bracerParser.parse("(((true | false) & not(false)) | (true | false))");
         Assert.assertEquals("1", bracerParser.evaluate());
 
-        Collection< String > stackRPN = bracerParser.getStackRPN();
+        Collection<String> stackRPN = bracerParser.getStackRPN();
         Assert.assertThat(stackRPN,
-            IsIterableContainingInOrder.contains(new String[] {"|", "|", "0", "1", "&", "not", "0", "|", "0", "1"}));
+                IsIterableContainingInOrder.contains("|", "|", "0", "1", "&", "not", "0", "|", "0", "1"));
     }
 }
